a0eb0bf55fe102f9db3697a706a1eed95b523db3
“Forest-Oxendine” <oxendine12198@gmail.com>
2023-04-25 23:57:45 -0500

    cleared mistakes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Project1 from './Project1';
 
-// Linked the pages together
+
+
 const NavBar = () => {
   return (
     <>
@@ -10,65 +10,3 @@ const NavBar = () => {
   }
 
 export default NavBar;
-
-
-// Pulled from Mood tracker app Project1
-// import React from 'react';
-// import styled from 'styled-components';
-// import {NavLink} from 'react-router-dom';
-
-// function NavBar (){
-//     return(
-// <>
-// <header>
-//     <Bar>
-//         <NavItem>
-//             <NavLink to="/" end>Home</NavLink>
-//         </NavItem>
-//         <NavItem>
-//             <NavLink to="/moodquestionspage">Info</NavLink>
-//         </NavItem>
-//         <NavItem>
-//             <NavLink to="/emotioncard">Mood List</NavLink>
-//         </NavItem>
-//     </Bar>
-// </header>
-
-// </>
-//     );
-// }
-// export default NavBar;
-
-// const Bar = styled.ul`
-// margin: 0;
-// padding: 0;
-// overflow: hidden;
-// background-color: #333;
-// `;
-
-// const NavItem = styled.li`
-// display: block;
-//     color: white;
-//     text-align: center;
-//     padding: 14px 16px;
-//     text-decoration: none;
-//     float: left;
-
-//     a{
-//         display: block;
-//         color: white;
-//         text-align: center;
-//         padding: 14px 16px;
-//         text-decoration: none;
-//     }
-
-//     a:hover:not(.active){
-//         background-color: #ADD8E6;
-//         color: #000000;
-//     }
-
-//     .active{
-//         background-color: #ADD8E6;
-//         color: #000000;
-//     }
-// `;
