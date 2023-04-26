e6c89504f0941eee3a4171dcfbd97f83e3fa737b
“Forest-Oxendine” <oxendine12198@gmail.com>
2023-04-25 23:52:33 -0500

    added what I accidentally committed on main for the aboutme component

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+// import Project1 from './components/Project1';
 
-// Linked the pages together
+
 const AboutMe = () => {
   return (
     <>
+    <Why>
+      'I found an ad on LinkedIn for those in marginalized communities to apply for a Full Stack Developers course with Hack the Gap.'
+    </Why>
+    <PrjOne>
+      'Project1'
+    </PrjOne>
     </>
   );
   }
 
 export default AboutMe;
+
+const Why = styled.h2`
+  font-size: 30px;
+`
+const PrjOne = styled.h1`
+  font-size: 40px;
+`
