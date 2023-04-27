224405098a55ca465ef94045c66b30628659e981
“Forest-Oxendine” <oxendine12198@gmail.com>
2023-04-26 20:54:19 -0500

    deleted accidental commits

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
-// import Project1 from './components/Project1';
+
 
 
 const AboutMe = () => {
   return (
     <>
-    <Why>
-      'I found an ad on LinkedIn for those in marginalized communities to apply for a Full Stack Developers course with Hack the Gap.'
-    </Why>
     <PrjOne>
       'Project1'
     </PrjOne>
@@ -18,9 +15,6 @@ const AboutMe = () => {
 
 export default AboutMe;
 
-const Why = styled.h2`
-  font-size: 30px;
-`
 const PrjOne = styled.h1`
   font-size: 40px;
 `
